1e3c0b3f1a67e748ff35befe107a00dbb012fa69
Madars Šmits <Madars.Smits@outlook.lv>
2018-12-21 14:37:37 +0200

    Unify all JSON templates for Workshops.

---
+++ scripts/lower-rdp-security.ps1
@@ -0,0 +1,2 @@
+$TargetServer = "S3-Veeam-VM"
+(Get-WmiObject -class "Win32_TSGeneralSetting" -Namespace root\cimv2\terminalservices -ComputerName $TargetServer -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0)
