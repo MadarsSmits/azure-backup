358071d98760d04b1bf1b3a597d21db59d6d8294
Madars Šmits <Madars.Smits@outlook.lv>
2018-12-21 15:16:47 +0200

    Unify all JSON templates for Workshops.

--- scripts/install-dc-dns.ps1
+++ scripts/install-dc-dns.ps1
@@ -1,3 +1,4 @@
 $password = ConvertTo-SecureString "P@ssW0rD!" -AsPlainText -Force
 Install-windowsfeature AD-domain-services
+Add-WindowsFeature RSAT-ADDS-Tools
 Install-ADDSForest -CreateDnsDelegation:$false -DatabasePath "C:\Windows\NTDS" -DomainMode "WinThreshold" -DomainName "valis2.local" -DomainNetbiosName "VALIS2" -ForestMode "WinThreshold" -InstallDns:$true -LogPath "C:\Windows\NTDS" -NoRebootOnCompletion:$false -SysvolPath "C:\Windows\SYSVOL" -Force:$true -SafeModeAdministratorPassword $password
